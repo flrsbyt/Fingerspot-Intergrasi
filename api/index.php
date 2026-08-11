ebf96924ef2037f0a6ed9b43b57069c3e49094f8
flrbyt <241108flora@gmail.com>
2026-08-11 10:01:46 +0700

    Fix: Perbaiki api/index.php untuk Vercel production

--- api/index.php
+++ api/index.php
@@ -1,3 +1,20 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
